74351b0c698bedea73f584152b12e8d8fd16154f
Lucas Gabriel Ferreira <100377156+LucasGF-Git@users.noreply.github.com>
2022-03-28 11:17:08 -0300

    ex009

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu  carro é ${vel}KM/h`)
+if (vel > 60) {
+    console.log(`Você ultrtapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cino de segurança!`)
